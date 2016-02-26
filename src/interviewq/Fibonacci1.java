233b03a8cc9257527d4ea8ec78e369b006f26b5f
Arun Patel <support@ydsworld.com>
2016-02-25 21:20:28 -0500

    fibonacci one in one loop

---
+++ src/interviewq/Fibonacci1.java
@@ -0,0 +1,24 @@
+package interviewq;
+
+/**
+ * Created by Arun on 2/25/16.
+ */
+public class Fibonacci1 {
+    public static void main(String args[]){
+
+       int f = 0;
+        int g = 1;
+
+
+
+        for (int i=1;i<=10;i++){
+
+            System.out.print(f + " ");
+            f = f + g;
+            g = f - g;
+
+        }
+
+        System.out.println();
+    }
+}
