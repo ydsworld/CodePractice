cded34e2eadea6fce51c404140d5ef0523ea1e02
Arun Patel <support@ydsworld.com>
2016-02-26 12:05:22 -0500

    fibonacchi

--- src/interviewq/Fibonacci.java
+++ src/interviewq/Fibonacci.java
@@ -1,20 +1,18 @@
 package interviewq;
 
-/**
- * Created by Arun on 2/25/16.
- */
+
 public class Fibonacci {
     public static void main(String args[]) {
 
-        int f = 0;
-        int g = 1;
+        int a = 0;
+        int b = 1;
 
 
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 20; i++) {
 
-            System.out.print(f + " ");
-            f = f + g;
-            g = f - g;
+            System.out.print(a + " ");
+            a = a + b;
+            b = a - b;
 
         }
 
