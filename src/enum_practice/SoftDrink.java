8d9e8c132c8bea199f47df64cab45e097fe58cd5
Arun Patel <support@ydsworld.com>
2016-03-17 23:32:50 -0400

    deleted old files

--- src/enum_practice/SoftDrink.java
+++
@@ -1,7 +0,0 @@
-package enum_practice;
-
-/**
- * Created by Arun on 3/17/16.
- */
-public enum SoftDrink {
-}
--- src/modifier/CommonConstants.java
+++
@@ -1,7 +0,0 @@
-package modifier;
-
-/**
- * Created by Arun on 3/17/16.
- */
-public class CommonConstants {
-}
