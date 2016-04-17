48ac5182eb934e7a78909839d7016e17ef60e9de
Arun Patel <support@ydsworld.com>
2016-04-17 13:44:37 -0400

    Test1

--- src/main/java/temp/Temp.java
+++ src/main/java/temp/Temp.java
@@ -5,7 +5,7 @@ public class Temp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Temp class
-		System.out.println("test intellij");
+		System.out.println("test intellij UI");
 	}
 
 }
