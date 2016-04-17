a54cdf56517238c8a74606ecf0d3341b74b564a2
Arun Patel <support@ydsworld.com>
2016-04-17 13:32:52 -0400

    Using IntelliJ to commit

--- src/main/java/temp/Temp.java
+++ src/main/java/temp/Temp.java
@@ -4,7 +4,8 @@ public class Temp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Temp class 
+		//Temp class
+		System.out.println("test intellij");
 	}
 
 }
