3c510cf0742f4e5e1408600965509dae7f51a30f
YDS World <support@ydsworld.com>
2018-02-03 12:18:47 -0500

    removed nnittest

--- src/main/java/json/JsonStringOjbectToMap.java
+++ src/main/java/json/JsonStringOjbectToMap.java
@@ -14,9 +14,9 @@ public class JsonStringOjbectToMap {
 
 
         String jsonStr = "[\n" +
-                "{ \"value\": \"http://publish1.nnittest.com:4503\", \"text\": \"Production Publish (http://publish1.nnittest.com:4503)\"},\n" +
-                "{ \"value\": \"http://publish.prb.nnittest.com:4503\", \"text\": \"PRB Publish (http://publish.prb.nnittest.com:4503)\"},\n" +
-                "{ \"value\": \"http://publish1.qa.nnittest.com:4503\", \"text\": \"QA Publish (http://publish1.qa.nnittest.com:4503)\"}\n" +
+                "{ \"value\": \"http://publish1.ydsworld.com:4503\", \"text\": \"Production Publish (http://publish1.ydsworld.com:4503)\"},\n" +
+                "{ \"value\": \"http://publish.prb.ydsworld.com:4503\", \"text\": \"PRB Publish (http://publish.prb.ydsworld.com:4503)\"},\n" +
+                "{ \"value\": \"http://publish1.qa.ydsworld.com:4503\", \"text\": \"QA Publish (http://publish1.qa.ydsworld.com:4503)\"}\n" +
                 "]";
 
 
