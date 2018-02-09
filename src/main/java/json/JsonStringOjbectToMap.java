ec6b7c3d89cd722b4ed222fb45412d5bdda22a3b
YDS World <support@ydsworld.com>
2018-02-09 18:52:36 -0500

    test commit

--- src/main/java/json/JsonStringOjbectToMap.java
+++ src/main/java/json/JsonStringOjbectToMap.java
@@ -14,9 +14,9 @@ public class JsonStringOjbectToMap {
 
 
         String jsonStr = "[\n" +
-                "{ \"value\": \"http://publish1.ydsworld.com:4503\", \"text\": \"Production Publish (http://publish1.ydsworld.com:4503)\"},\n" +
+                "{ \"value\": \"http://publish.ydsworld.com:4503\", \"text\": \"Production Publish (http://publish1.ydsworld.com:4503)\"},\n" +
                 "{ \"value\": \"http://publish.prb.ydsworld.com:4503\", \"text\": \"PRB Publish (http://publish.prb.ydsworld.com:4503)\"},\n" +
-                "{ \"value\": \"http://publish1.qa.ydsworld.com:4503\", \"text\": \"QA Publish (http://publish1.qa.ydsworld.com:4503)\"}\n" +
+                "{ \"value\": \"http://publish.qa.ydsworld.com:4503\", \"text\": \"QA Publish (http://publish1.qa.ydsworld.com:4503)\"}\n" +
                 "]";
 
 
